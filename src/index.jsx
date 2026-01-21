7c58f65ad33c865dc9152e5dbc467d85d6ad1f9f
leonardomv1981 <leonardo@leonardovilela.com>
2026-01-21 11:43:35 -0300

    delete ununsed files

--- src/index.jsx
+++ src/index.jsx
@@ -8,11 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 </React.StrictMode>
 );
-
-// import AppC from './AppC';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-// <React.StrictMode>
-//     <AppC />
-// </React.StrictMode>
-// );
--- src/indexCurso.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppC from './AppC';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-    <AppC />
-</React.StrictMode>
-);
